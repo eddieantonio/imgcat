d6e2726bff161fb1d7fd5c43f3fa200edd3dcb39
Eddie Antonio Santos <easantos@ualberta.ca>
2014-12-09 23:41:01 -0700

    Get terminal color info from tput(1).

--- imgcat.c
+++ imgcat.c
@@ -2,6 +2,7 @@
 #include <stdbool.h>
 #include <stdio.h>
 
+#include <unistd.h>
 #include <sys/ioctl.h>
 
 #include "stb_image.h"
@@ -17,6 +18,13 @@ static struct {
     bool should_resize; /* Default: yes! */
 } options;
 
+static struct {
+    int width;
+    int height;
+    int colors;
+    bool isatty;
+} terminal_info;
+
 void parse_args(int argc, char **argv);
 void print_image(const char *filename, int required_width, Format format);
 void determine_terminal_capabilities();
@@ -24,6 +32,11 @@ void determine_terminal_capabilities();
 int main(int argc, char **argv) {
     parse_args(argc, argv);
     determine_terminal_capabilities();
+
+    /* Quick test.... */
+    fprintf(stderr, "Winsize: %dx%d, %d color terminal\n", terminal_info.width,
+            terminal_info.height, terminal_info.colors);
+    fprintf(stderr, "stdout %s a tty.\n", terminal_info.isatty ? "is" : "is not");
 
     fprintf(stderr, "Not implemented!\n");
     return -1;
@@ -65,10 +78,28 @@ void print_image(const char *filename, int required_width, Format format) {
 }
 
 void determine_terminal_capabilities() {
+    FILE *tput;
     int stdout_fd = fileno(stdout);
     struct winsize ws;
-    ioctl(stdout_fd, TIOCGWINSZ, &ws);
-    printf("Winsize: %dx%d\n", ws.ws_col, ws.ws_row);
+    int colors;
+
+    if (!isatty(stdout_fd)) {
+        terminal_info.isatty = false;
+        return;
+    }
+
+    terminal_info.isatty = true;
+
+    /* Get the current window size. */
+    assert(ioctl(stdout_fd, TIOCGWINSZ, &ws) != -1);
+    terminal_info.width = ws.ws_col;
+    terminal_info.height = ws.ws_row;
+
+    /* I don't really know of a better way to do this other than invoke tput.  */
+    assert((tput = popen("tput colors", "r")) != NULL);
+    assert(fscanf(tput, "%d", &colors) == 1);
+    assert(pclose(tput) != -1);
+    terminal_info.colors = colors;
 }
 
 void parse_args(int argc, char **argv) {
