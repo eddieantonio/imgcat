50711a8ee4737f0c643926b86b1596d707c419d0
Eddie Antonio Santos <easantos@ualberta.ca>
2014-12-11 00:46:19 -0700

    Add simple resizing.

--- src/imgcat.c
+++ src/imgcat.c
@@ -1,11 +1,14 @@
 #include <assert.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #include <unistd.h>
 #include <sys/ioctl.h>
 
 #include "stb_image.h"
+#include "stb_image_resize.h"
+
 #include "rgbtree.h"
 
 
@@ -29,19 +32,14 @@ static struct {
 
 
 void parse_args(int argc, char **argv);
-void print_image(const char *filename, int required_width, Format format);
+void print_image(const char *filename, int max_width, Format format);
 void determine_terminal_capabilities();
 
 int main(int argc, char **argv) {
     parse_args(argc, argv);
     determine_terminal_capabilities();
 
-    /* Quick test.... */
-    fprintf(stderr, "Winsize: %dx%d, %d color terminal\n", terminal_info.width,
-            terminal_info.height, terminal_info.colors);
-    fprintf(stderr, "stdout %s a tty.\n", terminal_info.isatty ? "is" : "is not");
-
-    print_image(argv[1], 0, F_256_COLOR);
+    print_image(argv[1], terminal_info.width, F_256_COLOR);
 
     return 0;
 }
@@ -51,8 +49,9 @@ int main(int argc, char **argv) {
 void print_image_8(unsigned char *buffer);
 void print_image_256(unsigned char *buffer, int width, int height);
 void print_image_rgb(unsigned char *buffer);
+void do_resize(unsigned char **buffer, int new_width, int* width, int* height);
 
-void print_image(const char *filename, int required_width, Format format) {
+void print_image(const char *filename, int max_width, Format format) {
     int width, height, depth;
     /* Load with 3 components. */
     unsigned char *buffer = stbi_load(filename, &width, &height, &depth, 3);
@@ -60,6 +59,11 @@ void print_image(const char *filename, int required_width, Format format) {
     assert(depth == 3 && "Not the right colour depth!");
 
     /* Maybe do a resize. */
+    if (width > max_width) {
+        /* Warning: allocates a new buffer and frees the current one. */
+        do_resize(&buffer, max_width, &width, &height);
+    }
+    
 
     /* That resized buffer? Yeah. Print it. */
     switch (format) {
@@ -97,7 +101,7 @@ void determine_terminal_capabilities() {
     terminal_info.width = ws.ws_col;
     terminal_info.height = ws.ws_row;
 
-    /* I don't really know of a better way to do this other than invoke tput.  */
+    /* I don't really know of a better way to do this other than invoke tput. */
     assert((tput = popen("tput colors", "r")) != NULL);
     assert(fscanf(tput, "%d", &colors) == 1);
     assert(pclose(tput) != -1);
@@ -124,4 +128,22 @@ void print_image_256(unsigned char *buffer, int width, int height) {
         /* Finish the line. */
         printf("\033[49m\n");
     }
+}
+
+void do_resize(unsigned char **buffer, int new_width, int* width, int* height) {
+    unsigned char *original = *buffer, *resized;
+    double ratio = ((double) *height) / *width;
+    int new_height = ratio * new_width;
+    size_t buffer_size = 3 * (new_width * new_height);
+
+    resized = (unsigned char*) malloc(buffer_size);
+    stbir_resize_uint8(original, *width, *height, 0,
+                       resized, new_width, new_height, 0, 3);
+
+    /* Replace all the originals with our new, fun values. */
+    *buffer = resized;
+    *width = new_width;
+    *height = new_height;
+
+    stbi_image_free(original);
 }
