9242dc0380259ed6037dfc0f56c3e206ddcc3f63
Eddie Antonio Santos <easantos@ualberta.ca>
2014-12-12 12:55:34 -0700

    Make Image struct for buffer, width, height, depth.

--- src/imgcat.c
+++ src/imgcat.c
@@ -32,6 +32,12 @@ static struct {
     bool isatty;
 } terminal_info;
 
+typedef struct {
+    unsigned char *buffer;
+    int width;
+    int height;
+    int depth;
+} Image;
 
 
 void parse_args(int argc, char **argv);
@@ -49,24 +55,29 @@ int main(int argc, char **argv) {
 
 
 
-void print_image_8(unsigned char *buffer);
-void print_image_256(unsigned char *buffer, int width, int height);
-void print_image_rgb(unsigned char *buffer);
-void do_resize(unsigned char **buffer, int new_width, int* width, int* height);
+void print_image_8(Image *image);
+void print_image_256(Image *image);
+void print_image_rgb(Image *image);
+void reallocate_and_resize(Image *image, int new_width);
 
 void print_image(const char *filename, int max_width, Format format) {
-    int width, height, depth;
+    Image image;
     /* Load with 3 components. */
-    unsigned char *buffer = stbi_load(filename, &width, &height, &depth, 3);
+    unsigned char *buffer = stbi_load(filename,
+                                      &image.width,
+                                      &image.height,
+                                      &image.depth, 0);
 
-    assert(depth == 3 && "Not the right colour depth!");
+    /* TODO: Turn into a genuine image. */
+    assert(buffer != NULL && "Could not load image!");
+    image.buffer = buffer;
 
     /* Maybe do a resize. */
-    if (width > max_width) {
+    if (image.width > max_width) {
         /* Warning: allocates a new buffer and frees the current one. */
-        do_resize(&buffer, max_width, &width, &height);
+        reallocate_and_resize(&image, max_width);
     }
-    
+
 
     /* That resized buffer? Yeah. Print it. */
     switch (format) {
@@ -74,7 +85,7 @@ void print_image(const char *filename, int max_width, Format format) {
             assert(0 && "Not implemented!");
             break;
         case F_256_COLOR:
-            print_image_256(buffer, width, height);
+            print_image_256(&image);
             break;
         case F_RGB_COLOR:
             assert(0 && "Not implemented!");
@@ -117,13 +128,17 @@ void parse_args(int argc, char **argv) {
     options.should_resize = true;
 }
 
-void print_image_256(unsigned char *buffer, int width, int height) {
+void print_image_256(Image *image) {
     int x, y;
+    int width = image->width, height = image->height;
+    int color_depth = image->depth;
+    unsigned char *buffer = image->buffer;
+
     for (y = 0; y < height; y++) {
         /* Print each pixel. */
         for (x = 0; x < width; x++) {
             /* Get the position of the pixel in the image... */
-            uint8_t *pixel = buffer + 3 * (x + width * y);
+            uint8_t *pixel = buffer + color_depth * (x + width * y);
             const RGB_Node *match = rgb_closest_colour(pixel[0], pixel[1], pixel[2]);
             int closest_code = match->id;
             printf("\033[48;5;%03dm ", closest_code);
@@ -133,20 +148,23 @@ void print_image_256(unsigned char *buffer, int width, int height) {
     }
 }
 
-void do_resize(unsigned char **buffer, int new_width, int* width, int* height) {
-    unsigned char *original = *buffer, *resized;
-    double ratio = ((double) *height) / *width;
+void reallocate_and_resize(Image *image, int new_width) {
+    unsigned char *original = image->buffer;
+    unsigned char *resized;
+    double ratio = ((double) image->height) / image->width;
     int new_height = ratio * new_width;
-    size_t buffer_size = 3 * (new_width * new_height);
+    int color_depth = image->depth;
+
+    size_t buffer_size = color_depth * (new_width * new_height);
 
     resized = (unsigned char*) malloc(buffer_size);
-    stbir_resize_uint8(original, *width, *height, 0,
-                       resized, new_width, new_height, 0, 3);
+    stbir_resize_uint8(original, image->width, image->height, 0,
+                       resized, new_width, new_height, 0, color_depth);
 
     /* Replace all the originals with our new, fun values. */
-    *buffer = resized;
-    *width = new_width;
-    *height = new_height;
+    image->buffer = resized;
+    image->width = new_width;
+    image->height = new_height;
 
     stbi_image_free(original);
 }
