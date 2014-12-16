660498ac0249f6fe33132ed6f2fa97b02aa4e168
Eddie Antonio Santos <easantos@ualberta.ca>
2014-12-16 01:48:45 -0700

    Add 8 color mode.

--- src/print_image.c
+++ src/print_image.c
@@ -20,10 +20,24 @@ typedef struct {
     int depth;
 } Image;
 
-static void print_image_8(Image *image);
-static void print_image_256(Image *image);
+typedef const unsigned char Pixel;
+typedef void (*PixelFunc)(Pixel *pixel);
+
+static void image_iterator(Image *image, PixelFunc printer);
+static void printer_8_color(Pixel *pixel);
+static void printer_256_color(Pixel *pixel);
 static void print_image_rgb(Image *image);
 static void reallocate_and_resize(Image *image, int new_width);
+
+/* The 8 color table. It has 8 colors. */
+static const RGB_Tuple ansi_color_table[] = {
+    {{  0,   0,   0}}, {{ 128,   0,   0}},
+    {{  0, 128,   0}}, {{ 128, 128,   0}},
+    {{  0,   0, 128}}, {{ 128,   0, 128}},
+    {{  0, 128, 128}}, {{ 128, 128, 128}},
+};
+
+
 
 void print_image(const char *filename, int max_width, Format format) {
     Image image;
@@ -42,13 +56,15 @@ void print_image(const char *filename, int max_width, Format format) {
         reallocate_and_resize(&image, max_width);
     }
 
+    PixelFunc printer = NULL;
+
     /* That resized buffer? Yeah. Print it. */
     switch (format) {
         case F_8_COLOR:
-            assert(0 && "Not implemented!");
+            printer = printer_8_color;
             break;
         case F_256_COLOR:
-            print_image_256(&image);
+            printer = printer_256_color;
             break;
         case F_RGB_COLOR:
             assert(0 && "Not implemented!");
@@ -57,10 +73,45 @@ void print_image(const char *filename, int max_width, Format format) {
             assert(0 && "Not a valid format.");
     }
 
+    /* Print the image with the given pixel func. */
+    image_iterator(&image, printer);
+
     free(image.buffer);
 }
 
-void print_image_256(Image *image) {
+
+/**
+ * Gets a colour match from the global RGB tree.
+ */
+static void printer_256_color(Pixel *pixel) {
+    const RGB_Node *match = rgb_closest_colour(pixel[0], pixel[1], pixel[2]);
+    int closest_code = match->id;
+    printf("\033[48;5;%03dm ", closest_code);
+}
+
+/* Gets a color from the list. Will take same number of steps as the tree
+ * version. */
+static void printer_8_color(Pixel *pixel) {
+    RGB_Tuple target = { pixel[0], pixel[1], pixel[2] };
+    int i, best_index = 0;
+    unsigned int distance, closest;
+    closest = rgb_colour_distance(&ansi_color_table[0], &target);
+
+    for (i = 1; i < 8; i++) {
+        distance = rgb_colour_distance(&ansi_color_table[i], &target);
+        if (distance < closest) {
+            closest = distance;
+            best_index = i;
+        }
+    }
+
+    /* It turns out that the 8 color array has the SAME indices as its
+     * corresponding ANSI escape sequence. */
+    printf("\033[4%1dm ", best_index);
+}
+
+
+static void image_iterator(Image *image, PixelFunc printer) {
     int x, y;
     int width = image->width, height = image->height;
     int color_depth = image->depth;
@@ -71,16 +122,15 @@ void print_image_256(Image *image) {
         for (x = 0; x < width; x++) {
             /* Get the position of the pixel in the image... */
             uint8_t *pixel = buffer + color_depth * (x + width * y);
-            const RGB_Node *match = rgb_closest_colour(pixel[0], pixel[1], pixel[2]);
-            int closest_code = match->id;
-            printf("\033[48;5;%03dm ", closest_code);
+            printer(pixel);
         }
         /* Finish the line. */
         printf("\033[49m\n");
     }
 }
 
-void reallocate_and_resize(Image *image, int new_width) {
+
+static void reallocate_and_resize(Image *image, int new_width) {
     unsigned char *original = image->buffer;
     unsigned char *resized;
     double ratio = ((double) image->height) / image->width;
@@ -100,4 +150,3 @@ void reallocate_and_resize(Image *image, int new_width) {
 
     stbi_image_free(original);
 }
-
